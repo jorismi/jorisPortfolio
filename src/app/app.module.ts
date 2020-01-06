15397db6993b902e669c67811ac779d7d084da98
Your Name <you@example.com>
2020-01-07 00:34:00 +0100

    init architecture and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+
+import { WelcomeModule } from './welcome/welcome.module';
+import { SharedModule } from './shared/shared.module';
+import { PhotoModule } from './photo/photo.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,               
+    WelcomeModule,
+    SharedModule,
+    PhotoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
