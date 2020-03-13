f74407d7d6e875e833e97762d0339ecbc49bd384
Your Name <you@example.com>
2020-03-14 00:08:16 +0100

    some design on about section

--- src/app/about/about/experience.class.ts
+++ src/app/about/about/experience.class.ts
@@ -1,15 +1,31 @@
 export class Experience {
-    date: string;
+    beginDate: string;
+    endDate: string;
     title: string;
     location: string;
     description: string;
     keywords: string[];
+    yearBeginDate: string;
+    yearEndDate: string;
+    monthBeginDate: string;
+    monthEndDate: string;
+    dayOfMonthBeginDate: string;
+    dayOfMonthEndDate: string;
 
-    constructor(date: string, title: string, location: string, description: string, keywords: string[]) {
-        this.date = date;
+    constructor(beginDate: string, endDate: string, title: string, location: string, description: string, keywords: string[]) {
+        this.beginDate = beginDate;
+        this.endDate = endDate;
         this.title = title;
         this.location = location;
         this.description = description;
         this.keywords = Array.from(keywords);
+
+        // formatting date for display
+        this.yearBeginDate = beginDate.split("/")[2];
+        this.yearEndDate = endDate.split("/")[2];
+        this.monthBeginDate = new Intl.DateTimeFormat('fr-FR', { month: 'long' }).format(new Date(beginDate));
+        this.monthEndDate = new Intl.DateTimeFormat('fr-FR', { month: 'long' }).format(new Date(endDate));
+        this.dayOfMonthBeginDate = beginDate.split("/")[1];
+        this.dayOfMonthEndDate = endDate.split("/")[1];
     }
 }
